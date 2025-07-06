78c0be41c61ae6b4f6e05bf5d783df13202b285d
Muhammed Imdaad <muhammedimdaad16@gmai.com>
2025-07-06 14:00:18 +0530

    hello world end

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * In Go, a name is exported if it begins with a capital letter. 
 * A function can return any number of results.
 * Inside a function, the := short assignment statement can be used in place of a `var` declaration with implicit type.
+* Go has only one looping construct, the for loop. Once you drop the init and post statements it becomes a while. drop condition, it becomes an infinite loop. 
--- hello/hello.go
+++ hello/hello.go
@@ -5,17 +5,30 @@ import (
 	"rsc.io/quote" // `go mod tidy` will add the quote module as a requirement
 )
 
-const englishHelloPrefix = "Hello, " // Constants cannot be declared using the := syntax
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, " // Constants cannot be declared using the := syntax
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string { // type comes after the variable name.
+func Hello(name, language string) string { // type comes after the variable name.
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	switch language {
+	case spanish:
+		return spanishHelloPrefix + name
+	case french:
+		return frenchHelloPrefix + name
+	default:
+		return englishHelloPrefix + name
+	}
 }
 
 func main() { // main function, go run .
-	fmt.Println(Hello("imd"))
+	fmt.Println(Hello("imd", ""))
 	fmt.Println(quote.Go())
 }
